94f80b1f7e4aeb45a160e8fe44d7d016658c56ba
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-24 14:27:17 +0200

    implemente catalog mapper provider and catalog product profile.

---
+++ ClothingStoreFranchise.NetCore.Catalog/Mapper/CatalogMapperProvider.cs
@@ -0,0 +1,12 @@
+﻿using ClothingStoreFranchise.NetCore.Common.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingStoreFranchise.NetCore.Catalog.Mapper
+{
+    public class CatalogMapperProvider : BaseMapperProvider
+    {
+    }
+}
---
+++ ClothingStoreFranchise.NetCore.Catalog/Mapper/CatalogProductProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ClothingStoreFranchise.NetCore.Catalog.Dto;
+using ClothingStoreFranchise.NetCore.Catalog.Dto.Events;
+using ClothingStoreFranchise.NetCore.Catalog.Model;
+
+namespace ClothingStoreFranchise.NetCore.Catalog.Mapper
+{
+    public class CatalogProductProfile : Profile
+    {
+        public CatalogProductProfile()
+        {
+            CreateMap<CatalogProduct, CatalogProductDto>();
+
+            CreateMap<CatalogProductDto, CatalogProduct>()
+                .ForMember(entity => entity.CurrentOffer, p => p.Ignore())
+                .ForMember(entiy => entiy.Offers, p => p.Ignore())
+                .ForMember(entity => entity.CatalogProductId, p => p.Ignore());
+
+            CreateMap<CatalogProductDto, CreateProductEvent>();
+        }
+    }
+}
