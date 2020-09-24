6253cd2bdf9c75f430918f4e8ec66592087b443a
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-24 14:24:49 +0200

    implemented catalog product dao

---
+++ ClothingStoreFranchise.NetCore.Catalog/Dao/CatalogProductDao.cs
@@ -0,0 +1,12 @@
+﻿using ClothingStoreFranchise.NetCore.Catalog.Model;
+using ClothingStoreFranchise.NetCore.Common.EntityFramework;
+
+namespace ClothingStoreFranchise.NetCore.Catalog.Dao
+{
+    public class CatalogProductDao : BaseAbstractEntitiesDao<CatalogProduct, long, CatalogContext>, ICatalogProductDao
+    {
+        public CatalogProductDao(CatalogContext contextContainer) : base(contextContainer)
+        {
+        }
+    }
+}
---
+++ ClothingStoreFranchise.NetCore.Catalog/Dao/ICatalogProductDao.cs
@@ -0,0 +1,13 @@
+﻿using ClothingStoreFranchise.NetCore.Catalog.Model;
+using ClothingStoreFranchise.NetCore.Common.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingStoreFranchise.NetCore.Catalog.Dao
+{
+    public interface ICatalogProductDao : IDao<CatalogProduct, long>
+    {
+    }
+}
