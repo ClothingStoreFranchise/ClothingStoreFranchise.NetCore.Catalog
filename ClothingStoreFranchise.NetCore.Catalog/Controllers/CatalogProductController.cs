05da44fccb031633674b3a46edd07cee3f47d3de
jorgeagui <aguilera.go.jorge@gmail.com>
2020-11-03 15:08:31 +0100

    catalog controller setings and Category controller added.

--- ClothingStoreFranchise.NetCore.Catalog/Controllers/CatalogProductController.cs
+++ ClothingStoreFranchise.NetCore.Catalog/Controllers/CatalogProductController.cs
@@ -3,15 +3,12 @@ using ClothingStoreFranchise.NetCore.Catalog.Facade;
 using ClothingStoreFranchise.NetCore.Common.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace ClothingStoreFranchise.NetCore.Catalog.Controllers
 {
-    [Route("api/catalog")]
+    [Route("catalog_products")]
     public class CatalogProductController : ControllerBase
     {
         private readonly ICatalogProductService _catalogProductService;
@@ -21,17 +18,16 @@ namespace ClothingStoreFranchise.NetCore.Catalog.Controllers
             _catalogProductService = catalogProductService;
         }
 
-        [HttpGet("catalog_products")]
-        [Authorize(Policy = Policies.Admin)]
+        [HttpGet]
         public async Task<ActionResult<ICollection<CatalogProductDto>>> Get()
         {        //public IEnumerable<Customer> Get()
             return Ok(await _catalogProductService.LoadAllAsync());
         }
 
         [HttpPost]
-        public async Task Post([FromBody] CatalogProductDto catalogProductDto)
+        public async Task<ActionResult<CatalogProductDto>> Post([FromBody] CatalogProductDto catalogProductDto)
         {
-            await _catalogProductService.CreateProductAsync(catalogProductDto);
+            return Ok(await _catalogProductService.CreateAsync(catalogProductDto));
         }
     }
 }
---
+++ ClothingStoreFranchise.NetCore.Catalog/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using ClothingStoreFranchise.NetCore.Catalog.Dto;
+using ClothingStoreFranchise.NetCore.Catalog.Facade;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClothingStoreFranchise.NetCore.Catalog.Controllers
+{
+    [Route("category")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<CategoryDto>>> GetAll()
+        {
+            return Ok(await _categoryService.GetAllParentCategories());
+        }
+
+        [HttpGet("subcategory/{subcategoryid}")]
+        public async Task<ActionResult<ICollection<CatalogProductDto>>> GetSubcategoryProducts(long subcategoryid)
+        {
+            return Ok(await _categoryService.GetSubcategoryProducts(subcategoryid));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ICollection<CategoryDto>>> Post([FromBody] CategoryDto categoryDto)
+        {
+            await _categoryService.CreateAsync(categoryDto);
+            return Ok(await _categoryService.GetAllParentCategories());
+        }
+    }
+}
