cfd3a7b8b5d60fdd9df3bb3e45e7ac79c1fb75b1
jorgeagui <aguilera.go.jorge@gmail.com>
2020-11-03 15:06:04 +0100

    Catalog product progile and category profile traking added.

--- ClothingStoreFranchise.NetCore.Catalog/Mapper/CatalogProductProfile.cs
+++ ClothingStoreFranchise.NetCore.Catalog/Mapper/CatalogProductProfile.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using ClothingStoreFranchise.NetCore.Catalog.Dao;
 using ClothingStoreFranchise.NetCore.Catalog.Dto;
 using ClothingStoreFranchise.NetCore.Catalog.Dto.Events;
 using ClothingStoreFranchise.NetCore.Catalog.Model;
+using ClothingStoreFranchise.NetCore.Common.Constants;
 
 namespace ClothingStoreFranchise.NetCore.Catalog.Mapper
 {
@@ -13,10 +15,33 @@ namespace ClothingStoreFranchise.NetCore.Catalog.Mapper
 
             CreateMap<CatalogProductDto, CatalogProduct>()
                 .ForMember(entity => entity.CurrentOffer, p => p.Ignore())
-                .ForMember(entiy => entiy.Offers, p => p.Ignore())
-                .ForMember(entity => entity.CatalogProductId, p => p.Ignore());
+                .ForMember(entiy => entiy.Offers, p => p.Ignore());
+                
 
-            CreateMap<CatalogProductDto, CreateProductEvent>();
+            CreateMap<CatalogProductDto, CreateProductEvent>()
+                .AfterMap<TrackCatalogProductAction>();
+                //.ForMember(dest => dest.TypeClothingSize, opt => opt.MapFrom(src => src.Subcategory.TypeClothingSize));
+        }
+    }
+    public class TrackCatalogProductAction : IMappingAction<CatalogProductDto, CreateProductEvent>
+    {
+        private readonly ICategoryDao _categoryDao;
+
+        public TrackCatalogProductAction(ICategoryDao categoryDao) 
+        {
+            _categoryDao = categoryDao;
+        }
+
+
+        public void Process(CatalogProductDto source, CreateProductEvent destination, ResolutionContext context)
+        {
+            TrackCategory(source, destination);
+        }
+
+        public void TrackCategory(CatalogProductDto dto, CreateProductEvent @event)
+        {
+            Category b = _categoryDao.Load(dto.SubcategoryId);
+            @event.ClothingSizeType = (ClothingSizeType)b.TypeClothingSize;
         }
     }
 }
---
+++ ClothingStoreFranchise.NetCore.Catalog/Mapper/CategoryProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using ClothingStoreFranchise.NetCore.Catalog.Dao;
+using ClothingStoreFranchise.NetCore.Catalog.Dto;
+using ClothingStoreFranchise.NetCore.Catalog.Facade;
+using ClothingStoreFranchise.NetCore.Catalog.Model;
+
+namespace ClothingStoreFranchise.NetCore.Catalog.Mapper
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+
+            CreateMap<Category, CategoryDto>();
+            // Dto --> Entity
+            CreateMap<CategoryDto, Category>();
+        }
+    }
+
+    /*public class TrackCategoryAction : IMappingAction<CategoryDto, Category>
+    {
+        private readonly ICategoryDao _categoryDao;
+
+        public TrackCategoryAction(ICategoryDao categoryDao)
+        {
+            _categoryDao = categoryDao;
+        }
+
+
+        public void Process(CategoryDto dto, Category entity, ResolutionContext context)
+        {
+        }
+    }*/
+}
